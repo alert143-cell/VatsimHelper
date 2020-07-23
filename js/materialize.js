570c060afa9545e17c964148536a64dbd493a02f
FlightSimmerFan <61103421+alert143-cell@users.noreply.github.com>
2020-07-23 10:42:19 -0700

    Update materialize.js

--- js/materialize.js
+++ js/materialize.js
@@ -12372,3 +12372,9 @@ $jscomp.polyfill = function (e, r, p, m) {
 
   Range.init($('input[type=range]'));
 })(cash, M.anime);
+
+$(document).ready(function () {
+  $('.collapsible').collapsible({
+      accordion: false
+  });
+});
