03723c0c4df1115a5dd427fca8ad729348038aa9
FlightSimmerFan <61103421+alert143-cell@users.noreply.github.com>
2020-07-23 21:41:44 -0700

    Update

--- js/materialize.js
+++ js/materialize.js
@@ -12383,3 +12383,8 @@ document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.sidenav');
   var instances = M.Sidenav.init(elems);
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.dropdown-trigger');
+  var instances = M.Dropdown.init(elems, options);
+});
